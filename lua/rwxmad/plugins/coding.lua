5e39bbe85e9665681003a36b893dd7e2c74b863e
rwxmad <rwxmad@gmail.com>
2024-12-04 04:03:26 +0300

    feat(neogen): add neogen, generate annotations

--- lua/rwxmad/plugins/coding.lua
+++ lua/rwxmad/plugins/coding.lua
@@ -25,4 +25,19 @@ return {
     },
     config = true,
   },
+
+  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
+  {
+    'danymat/neogen',
+    keys = {
+      {
+        '<leader>cc',
+        function()
+          require('neogen').generate({})
+        end,
+        desc = 'Neogen Comment',
+      },
+    },
+    opts = { snippet_engine = 'luasnip' },
+  },
 }
