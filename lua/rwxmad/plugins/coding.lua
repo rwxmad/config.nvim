0ea65edc31558d1e9081ece12d46907a9d3bee83
rwxmad <rwxmad@gmail.com>
2024-12-04 03:59:06 +0300

    feat(inc-rename): add inc-rename.nvim

--- lua/rwxmad/plugins/coding.lua
+++ lua/rwxmad/plugins/coding.lua
@@ -6,4 +6,23 @@ return {
     opts = {},
     enabled = vim.fn.has('nvim-0.10') == 1,
   },
+
+  -- Incremental rename
+  {
+    'smjonas/inc-rename.nvim',
+    cmd = 'IncRename',
+    keys = {
+      {
+        '<leader>rn',
+        function()
+          return ':IncRename '
+        end,
+        mode = 'n',
+        noremap = true,
+        silent = true,
+        expr = true,
+      },
+    },
+    config = true,
+  },
 }
