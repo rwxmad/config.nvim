7110cfa590053c66c63e02a733fa430f6fc9f4b3
rwxmad <rwxmad@gmail.com>
2024-12-13 12:58:18 +0300

    feat: add `snacks.nvim`, also get dashboard from this plugin

--- lua/rwxmad/plugins/ui.lua
+++ lua/rwxmad/plugins/ui.lua
@@ -64,49 +64,106 @@ return {
   },
 
   {
-    'nvimdev/dashboard-nvim',
-    lazy = false,
-    config = function()
-      local logo = [[
+    'folke/snacks.nvim',
+    opts = {
+      dashboard = {
+        sections = {
+          { section = 'header' },
+          { section = 'keys', gap = 1, padding = 1 },
+          {
+            pane = 2,
+            icon = ' ',
+            desc = 'Browse Repo',
+            padding = 1,
+            key = 'r',
+            action = function()
+              Snacks.gitbrowse()
+            end,
+          },
+          function()
+            local in_git = Snacks.git.get_root() ~= nil
+            local cmds = {
+              {
+                title = 'Notifications',
+                cmd = 'gh notify -s -a -n5',
+                action = function()
+                  vim.ui.open('https://github.com/notifications')
+                end,
+                key = 'n',
+                icon = ' ',
+                height = 5,
+                enabled = true,
+              },
+              {
+                title = 'Open Issues',
+                cmd = 'gh issue list -L 3',
+                key = 'i',
+                action = function()
+                  vim.fn.jobstart('gh issue list --web', { detach = true })
+                end,
+                icon = ' ',
+                height = 7,
+              },
+              {
+                icon = ' ',
+                title = 'Open PRs',
+                cmd = 'gh pr list -L 3',
+                key = 'p',
+                action = function()
+                  vim.fn.jobstart('gh pr list --web', { detach = true })
+                end,
+                height = 7,
+              },
+              {
+                icon = ' ',
+                title = 'Git Status',
+                cmd = 'hub --no-pager diff --stat -B -M -C',
+                height = 10,
+              },
+            }
+            return vim.tbl_map(function(cmd)
+              return vim.tbl_extend('force', {
+                pane = 2,
+                section = 'terminal',
+                enabled = in_git,
+                padding = 1,
+                ttl = 5 * 60,
+                indent = 3,
+              }, cmd)
+            end, cmds)
+          end,
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+        preset = {
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 'b', desc = 'File browser', action = ':Telescope file_browser' },
+            { icon = ' ', key = 'o', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = ' ',
+              key = 'c',
+              desc = 'Config',
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
+          header = [[
 ██████╗ ██╗    ██╗██╗  ██╗███╗   ███╗ █████╗ ██████╗
 ██╔══██╗██║    ██║╚██╗██╔╝████╗ ████║██╔══██╗██╔══██╗
 ██████╔╝██║ █╗ ██║ ╚███╔╝ ██╔████╔██║███████║██║  ██║
 ██╔══██╗██║███╗██║ ██╔██╗ ██║╚██╔╝██║██╔══██║██║  ██║
 ██║  ██║╚███╔███╔╝██╔╝ ██╗██║ ╚═╝ ██║██║  ██║██████╔╝
 ╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝
-]]
-
-      logo = string.rep('\n', 8) .. logo .. '\n\n'
-
-      require('dashboard').setup({
-        config = {
-          header = vim.split(logo, '\n'),
-          shortcut = {
-            {
-              desc = '  Recently',
-              action = 'Telescope oldfiles',
-              key = 'o',
-            },
-            {
-              desc = '  Files',
-              action = 'Telescope find_files find_command=rg,--hidden,--files',
-              key = 'f',
-            },
-            {
-              desc = '  Browser',
-              action = 'Telescope file_browser',
-              key = 'b',
-            },
-            {
-              desc = '  Grep',
-              action = 'Telescope live_grep',
-              key = 'g',
-            },
-          },
-          footer = {},
+]],
         },
-      })
-    end,
+      },
+    },
   },
 
   -- animations
