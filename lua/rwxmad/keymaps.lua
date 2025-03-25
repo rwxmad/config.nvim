2f7854444d7e284fc7078b9d65152af4ae8fda89
rwxmad <rwxmad@gmail.com>
2025-03-25 17:47:08 +0300

    feat(keymaps): add description for `vim.diagnostic` keymaps

--- lua/rwxmad/keymaps.lua
+++ lua/rwxmad/keymaps.lua
@@ -84,11 +84,11 @@ local diagnostic_goto = function(next, severity)
   end
 end
 
-vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Line diagnostics' })
 
-vim.keymap.set('n', ']d', diagnostic_goto(true))
-vim.keymap.set('n', '[d', diagnostic_goto(false))
-vim.keymap.set('n', ']e', diagnostic_goto(true, 'ERROR'))
-vim.keymap.set('n', '[e', diagnostic_goto(false, 'ERROR'))
-vim.keymap.set('n', ']w', diagnostic_goto(true, 'WARN'))
-vim.keymap.set('n', '[w', diagnostic_goto(false, 'WARN'))
+vim.keymap.set('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
+vim.keymap.set('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
+vim.keymap.set('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
+vim.keymap.set('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
+vim.keymap.set('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
+vim.keymap.set('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
