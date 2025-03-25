3cfe5bdf61f55427faebdbded9db9679ed03266d
rwxmad <rwxmad@gmail.com>
2025-03-25 17:39:53 +0300

    feat(keymaps): add keymaps for `vim.diagnostic`

--- lua/rwxmad/keymaps.lua
+++ lua/rwxmad/keymaps.lua
@@ -76,6 +76,19 @@ vim.keymap.set('n', '<leader>hb', '<cmd>Gitsigns blame_line<CR>')
 -- Diagnostics
 -- -------------------------------------------------------------------------------------------------
 
-vim.keymap.set('n', '<leader>ld', function()
-  vim.diagnostic.open_float()
-end)
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float)
+
+vim.keymap.set('n', ']d', diagnostic_goto(true))
+vim.keymap.set('n', '[d', diagnostic_goto(false))
+vim.keymap.set('n', ']e', diagnostic_goto(true, 'ERROR'))
+vim.keymap.set('n', '[e', diagnostic_goto(false, 'ERROR'))
+vim.keymap.set('n', ']w', diagnostic_goto(true, 'WARN'))
+vim.keymap.set('n', '[w', diagnostic_goto(false, 'WARN'))
