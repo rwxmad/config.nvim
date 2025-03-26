08faeeecbfd9ff5d8e70ea58beed6a372067efca
rwxmad <rwxmad@gmail.com>
2025-03-26 19:26:42 +0300

    feat(noice.nvim): add overrides to lsp section

--- lua/rwxmad/plugins/ui.lua
+++ lua/rwxmad/plugins/ui.lua
@@ -22,6 +22,13 @@ return {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
       presets = {
         bottom_search = true,
         command_palette = true,
