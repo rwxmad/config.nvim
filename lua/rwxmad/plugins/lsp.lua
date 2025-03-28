36dbee39e49179e6a61afed3e8fa90f5b9bc169b
rwxmad <rwxmad@gmail.com>
2025-03-28 17:59:55 +0300

    feat(lazydev.nvim): add `Snacks` to library section

--- lua/rwxmad/plugins/lsp.lua
+++ lua/rwxmad/plugins/lsp.lua
@@ -279,6 +279,7 @@ return {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
+        { path = 'snacks.nvim', words = { 'Snacks' } },
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
