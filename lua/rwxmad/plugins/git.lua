3cccd08b1dd9cb3d0ac91457adac99331c932559
rwxmad <rwxmad@gmail.com>
2025-04-03 16:43:59 +0300

    feat(keymaps): move `Gitsigns` keymaps from `keymaps` to `Gitsigns` configuration

--- lua/rwxmad/plugins/git.lua
+++ lua/rwxmad/plugins/git.lua
@@ -1,6 +1,16 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
+    keys = {
+      {
+        { '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>', desc = 'Stage hunk' },
+        { '<leader>hy', '<cmd>Gitsigns undo_stage_hunk<CR>', desc = 'Undo stage hunk' },
+        { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Reset hunk' },
+        { '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>', desc = 'Preview hunk' },
+        { '<leader>hb', '<cmd>Gitsigns blame_line<CR>', desc = 'Blame line' },
+        { '<leader>hd', '<cmd>Gitsigns diffthis<CR>', desc = 'Diff hunk' },
+      },
+    },
     config = function()
       require('gitsigns').setup()
     end,
