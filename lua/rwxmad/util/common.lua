481323ab5847e3b13fecce90f12ec32b9c1a3492
rwxmad <rwxmad@gmail.com>
2025-05-23 11:05:19 +0300

    feat(snippets): add ability to pass table with multiple langs for add snippet

--- lua/rwxmad/util/common.lua
+++ lua/rwxmad/util/common.lua
@@ -6,11 +6,17 @@ local i = ls.insert_node
 
 local fmt = require('luasnip.extras.fmt').fmt
 
--- NOTE: add snippets with one node
-function M.add_snippet(lang, trigger, fmt_string)
-  ls.add_snippets(lang, {
-    s(trigger, fmt(fmt_string, { i(0) })),
-  })
+function M.add_snippet(langs, trigger, fmt_string)
+  -- Convert single filetype string to table for uniform handling
+  if type(langs) == 'string' then
+    langs = { langs }
+  end
+
+  for _, lang in ipairs(langs) do
+    ls.add_snippets(lang, {
+      s(trigger, fmt(fmt_string, { i(0) })),
+    })
+  end
 end
 
 return M
