b4a7668044dc8158c20928f9fbda9be7f323e278
rwxmad <rwxmad@gmail.com>
2025-05-25 16:45:59 +0300

    chore(lsp): update option in `mason-lspconfig` setup

--- lua/rwxmad/plugins/lsp.lua
+++ lua/rwxmad/plugins/lsp.lua
@@ -237,6 +237,7 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = server_list,
         automatic_installation = true,
+        automatic_enable = true,
       })
 
       local ensure_installed = {
