ee732d56e16436b10b3083844270f81122b08370
rwxmad <rwxmad@gmail.com>
2025-07-24 12:28:24 +0300

    feat: remove `telescope.lua`, fix `keys` for `todo-comments.nvim`, search todo via `Snacks.picker.todo_comments()`

--- lua/rwxmad/plugins/editor.lua
+++ lua/rwxmad/plugins/editor.lua
@@ -25,8 +25,9 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<leader>st", function ()
+        Snacks.picker.todo_comments()
+      end, desc = "Todo" },
     },
   },
 }
