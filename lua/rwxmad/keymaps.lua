51c763711254ba20f12e0bb1227778327f29d451
rwxmad <rwxmad@gmail.com>
2025-07-29 21:29:25 +0300

    feat(keymaps): add useful keymaps for movement, windows, move lines

--- lua/rwxmad/keymaps.lua
+++ lua/rwxmad/keymaps.lua
@@ -7,24 +7,52 @@ vim.g.mapleader = ','
 
 -- Keymaps
 
+local map = vim.keymap.set
+
 -- Exit insert mode
-vim.keymap.set('i', 'jj', '<Esc>')
+map('i', 'jj', '<Esc>')
 
 -- Write file like :w command
-vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
+map('n', '<leader>w', '<cmd>w<CR>')
 
 -- Quit nvim like :q command
-vim.keymap.set('n', '<leader>q', '<cmd>q<CR>', { silent = true })
+map('n', '<leader>q', '<cmd>q<CR>', { silent = true })
 
 -- Turn off search highlight
-vim.keymap.set('n', '<leader><space>', '<cmd>nohlsearch<CR>')
+map('n', '<leader><space>', '<cmd>nohlsearch<CR>')
 
 -- Delete a word backwards
-vim.keymap.set('n', 'db', 'vb"_d')
+map('n', 'db', 'vb"_d')
 
 -- Move to start or end of line in insert mode
-vim.keymap.set('i', '<C-l>', '<ESC>A')
-vim.keymap.set('i', '<C-h>', '<ESC>I')
+map('i', '<C-l>', '<ESC>A')
+map('i', '<C-h>', '<ESC>I')
+
+-- Better up/down
+map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map('n', '<c-h>', '<c-w>h', { desc = 'go to left window', remap = true })
+map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
+map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
+map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+
+-- Move lines
+map('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
+map('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
+map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+map('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
+map('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
 
 -- -------------------------------------------------------------------------------------------------
 -- Diagnostics
@@ -38,11 +66,11 @@ local diagnostic_goto = function(next, severity)
   end
 end
 
-vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Line diagnostics' })
+map('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Line diagnostics' })
 
-vim.keymap.set('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
-vim.keymap.set('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
-vim.keymap.set('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
-vim.keymap.set('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
-vim.keymap.set('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
-vim.keymap.set('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
+map('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
+map('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
+map('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
+map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
+map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
+map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
