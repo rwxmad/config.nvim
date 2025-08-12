d3e9ef1a5558c67e32f1fd2d54324470864936db
rwxmad <rwxmad@gmail.com>
2025-08-12 23:59:46 +0300

    feat(mini): update configs for `mini` packages

--- lua/rwxmad/plugins/mini.lua
+++ lua/rwxmad/plugins/mini.lua
@@ -1,13 +1,55 @@
 return {
+  -- Autopairs
   {
-    'echasnovski/mini.nvim',
+    'echasnovski/mini.pairs',
+    event = 'VeryLazy',
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { 'string' },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    },
+    config = function(_, opts)
+      rwxmad.util.mini.pairs(opts)
+    end,
+  },
+  -- Work with surrounding
+  {
+    'echasnovski/mini.surround',
+    opts = {
+      mappings = {
+        add = 'gsa', -- Add surrounding in Normal and Visual modes
+        delete = 'gsd', -- Delete surrounding
+        find = 'gsf', -- Find surrounding (to the right)
+        find_left = 'gsF', -- Find surrounding (to the left)
+        highlight = 'gsh', -- Highlight surrounding
+        replace = 'gsr', -- Replace surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      require('mini.surround').setup(opts)
+    end,
+  },
+  -- Go forward/backward with square brackets
+  {
+    'echasnovski/mini.bracketed',
+    event = 'BufReadPost',
     config = function()
-      require('mini.pairs').setup({
-        mappings = {
-          ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\`].', register = { cr = false } },
-        },
+      local bracketed = require('mini.bracketed')
+      bracketed.setup({
+        file = { suffix = '' },
+        window = { suffix = '' },
+        quickfix = { suffix = '' },
+        yank = { suffix = '' },
+        treesitter = { suffix = 'n' },
       })
-      require('mini.surround').setup()
     end,
   },
 }
