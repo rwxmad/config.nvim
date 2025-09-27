5513a98b0d44f157221b792ddbd75b44d0e8d777
rwxmad <rwxmad@gmail.com>
2025-09-27 19:31:02 +0300

    feat(util): add `treesitter` module

--- lua/rwxmad/init.lua
+++ lua/rwxmad/init.lua
@@ -4,6 +4,7 @@ M = {
   defaults = require('rwxmad.defaults'),
   util = {
     common = require('rwxmad.util.common'),
+    treesitter = require('rwxmad.util.treesitter'),
     lsp = require('rwxmad.util.lsp'),
     cmp = require('rwxmad.util.cmp'),
     mini = require('rwxmad.util.mini'),
