b2ce4683bb4bfd54f4fb668ef7408e30d9a70679
rwxmad <rwxmad@gmail.com>
2026-02-21 10:42:18 +0300

    feat: add `obsidian.nvim` plugin

---
+++ lua/rwxmad/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    ui = {
+      enable = false,
+    },
+    workspaces = {
+      {
+        name = 'notes',
+        path = '~/notes',
+      },
+    },
+    templates = {
+      folder = '_templates',
+      -- date_format = '%Y-%m-%d',
+      -- time_format = '%H:%M',
+      -- A map for custom variables, the key should be the variable and the value a function
+      -- substitutions = {},
+    },
+  },
+}
