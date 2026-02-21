fe55182ab7b7420252f6e9e9afe6d2c1e0051770
rwxmad <rwxmad@gmail.com>
2026-02-21 11:52:56 +0300

    chore(statusline): change `lualine` theme to `gruvbox`

--- lua/rwxmad/plugins/statusline.lua
+++ lua/rwxmad/plugins/statusline.lua
@@ -24,7 +24,7 @@ return {
       return {
         options = {
           icons_enabled = true,
-          theme = 'tokyonight',
+          theme = 'gruvbox',
           section_separators = '',
           component_separators = '',
           disabled_filetypes = {},
